a598e8f5abd8cf61519642674bfc67d96e4f6a50
Nivk0 <98447362+Nivk0@users.noreply.github.com>
2022-02-04 04:22:24 -0600

    new home page

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "nivedh.koya@gmail.com";
+$sitename = "Nivedh";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
